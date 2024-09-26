0c841a89e14ff7382132ad9bee04b4d3dc76bc89
Lina <juicelina.com@gmail.com>
2024-09-26 12:35:21 +0300

    fixed task 2

--- js_4/hm6Task2.js
+++ js_4/hm6Task2.js
@@ -8,20 +8,18 @@
 
 const competitorsPizzas = ['Peperoni', 'Caprichosa', 'Diablo', '4 cheeses', 'hawai']
 // const myPizzas = ['peperoni', 'caprichosa', '4 cheeses']
-const myPizzas = ['Peperoni', 'Caprichosa', 'Diablo', '4 cheeses', 'hawai' ]
-comparePizzasWithCompetitors(myPizzas)
+const myPizzas = ['PeperonI', 'Caprichosa', 'diablo', '4 cheeses', 'hawai' ]
+comparePizzasWithCompetitors(myPizzas, competitorsPizzas)
 
-function comparePizzasWithCompetitors(arr){
+function comparePizzasWithCompetitors(arr, arr2){
     const newMyPizzas = []
-
-    for (let i = 0; i < myPizzas.length; i++) {
-        myPizzas[i] = myPizzas[i].toLowerCase();
+    for (let i = 0; i < arr.length; i++) {
+        arr[i] = arr[i].toLowerCase();
     }
-    for (let pizza of competitorsPizzas)  {
+    for (let pizza of arr2)  {
         if (!(arr.includes(pizza.toLowerCase()))){
             newMyPizzas.push(pizza)
         }  
     }
-
     console.log(newMyPizzas.length === 0 ? null: newMyPizzas)
 }
