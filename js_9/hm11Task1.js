c8d0a871e22e41e4d86883f28d59356efbc23e8e
Lina <juicelina.com@gmail.com>
2024-10-19 21:01:31 +0300

    hm 11

--- js_9/hm11Task1.js
+++ js_9/hm11Task1.js
@@ -1,3 +1,165 @@
 "use strict";
 
 
+// 1. Создайте класс Employee с полями: firstName - имя сотрудника (строка), lastName - фамилия сотрудника (строка),
+// profession - професси сотрудника (строка), приватное поле salary - зарплата сотрудника (число)
+// 2. Реализуйте геттеры и сеттеры для всех полей, включая приватное поле salary
+// 3. Реализуйе метод getFullName() - возвращающий полное имя сотрудника
+// 4. Проверьте корректную работу класс, создав несколько экземпляров и протестировав геттеры и сеттеры:
+// const emp1 = new Employee('John', 'Doe', 'Developer', 3000)
+// console.log(emp1.firstName) // 'John
+// emp1.salary = 3100
+// console.log(emp1.salary) // 3100
+// 5. Создайте класс Company с полями: title - название компании (строка), phone - телефон компании (число), addredd - адрес
+// компании (строка), приватное поле employees - массив сотрудников (пустой массив на старте)
+// 6. Реализуйте геттеры для полей title, phone, address
+// 7. Добавьте методы: 
+// - addEmployee(employee) - добавляет сотрудника в массив employees с проверкой, что переданный объект является экземпляром
+// класса Employee (instanceOf)
+// - getEmployees() - возвращает массив всех сотрудников
+// 8. Проверьте корректную работу:
+// const company = new Company('Tech Corp', '123-456', 'Main Street')
+// const emp1 = new Employee('John', 'Doe', 'Dev', 3000)
+// const emp2 = new Employee('Barbara', 'Johnson', 'QA', 2500)
+// company.addEmployee(emp1)
+// company.addEmployee(emp2)
+// console.log(company.getEmployees()) // [Employee, Employee]
+// 9. Добавьте в класс Company метод getInfo(), который возвращает строку с информацией о компании вида (перенос строки
+// сделать с \n):
+// Компания:
+// Адрес:
+// Количество сотрудников:
+
+
+class Employee {
+    #salary
+    constructor(firstName, lastName, profession, salary) {
+        this.firstName = firstName; // вызывается сеттер
+        this.lastName = lastName; 
+        this.profession = profession; 
+        this.salary = salary; 
+    }
+
+    get firstName(){
+        return this._firstName
+    }
+
+    set firstName(firstName){
+        if (typeof firstName != 'string'){
+            throw new Error('Invalid firstName value')
+        }
+        this._firstName = firstName
+    }
+
+    get lastName(){
+        return this._lastName
+    }
+
+    set lastName(lastName){
+        if (typeof lastName != 'string'){
+            throw new Error('Invalid lastName value')
+        }
+        this._lastName = lastName
+    }
+
+    get profession(){
+        return this._profession
+    }
+
+    set profession(profession){
+        if (typeof profession != 'string'){
+            throw new Error('Invalid profession value')
+        }
+        this._profession = profession
+    }
+
+    get salary(){
+        return this.#salary
+    }
+
+    set salary(salary){
+        if (typeof salary != 'number'){
+            throw new Error('Invalid salary value')
+        }
+        this.#salary = salary
+    }
+
+    get fullName(){
+        return `Name: ${this.firstName}, Surname: ${this.lastName}`
+    } 
+}
+
+class Company {
+    #employees = []
+    constructor(title, phone, address, employees){
+        this.title = title
+        this.phone = phone
+        this.address = address
+        this.employees = employees
+    }
+
+    get title(){
+        return this._title
+    }
+
+    set title(title){
+        if (typeof title != 'string'){
+            throw new Error('Invalid title value')
+        }
+        this._title = title
+    }
+
+    get phone(){
+        return this._phone
+    }
+
+    set phone(phone){
+        if (typeof phone != 'number'){
+            throw new Error('Invalid phone value')
+        }
+        this._phone = phone
+    }
+
+    get address(){
+        return this._address
+    }
+
+    set address(address){
+        if (typeof address != 'string'){
+            throw new Error('Invalid address value')
+        }
+        this._address = address
+    }
+
+    addEmployee(employee){
+        if (employee instanceof Employee){
+            this.#employees.push(employee)
+        }
+        else {
+            return `object ${employee} is not the instance of class Employee`
+        }
+
+    }
+
+    getEmployees(){
+        return this.#employees
+    }
+
+    getInfo(){
+        return `Компания: ${this.title} \nАдрес: ${this.address} \nКоличество сотрудников: ${this.#employees.length}`
+    }
+}
+
+
+// const company = new Company('Tech Corp', '123-456', 'Main Street') // вызывается ошибка, тк тип phone должен быть number!
+const company = new Company('Tech Corp', 123456, 'Main Street') // 
+console.log(company.phone) // 123456
+const emp1 = new Employee('John', 'Doe', 'Dev', 3000)
+console.log(emp1.firstName) // John
+emp1.salary = 3100
+console.log(emp1.salary) // 3100
+const emp2 = new Employee('Barbara', 'Johnson', 'QA', 2500)
+company.addEmployee(emp1)
+company.addEmployee(emp2)
+console.log(company.getEmployees()) // [Employee, Employee]
+console.log(company.getInfo()) 
---
+++ js_9/hm11Task2.js
@@ -0,0 +1,195 @@
+"use strict";
+
+
+// 1. Реализуйте метод findEmployeeByName(firstName: string) в классе Company, который возвращает объект сотрудника, если он найден
+// 2. Реализуйте метод removeEmployee(firstName) в классе Company, который удаляет сотрудника по имени
+// Метод должен исать сотрудника по имени и, если сотрудник найден, удалять его из массива
+// Для корректной работы создайте доп приватный метод getEmployeeIndex(firstName), и используйте его в removeEmployee
+// 3. Добавьте метод getTotalSalary() в классе Company, коотрый возвращает сумму всех зарплат сотрудников
+// 4. Добвление валидации для полей сотрудника в сеттеры:
+// Employee: 
+// firstName и lasName - строка от 2 до 50 символов, только латинские буквы
+// profession - строка, не может быть пустой, только латинские буквы и пробелы 
+// salary - число, должно быть больше 0 и меньше 100000
+// 5. Проверьте свой код:
+// const emp1 = new Employee('John', 'Doe', "Developer", 3000)
+// const emp2 =  new Employee('Jane', 'Smith', "Manager", 5000)
+// const emp3 =  new Employee('Mark', 'Brown', "Designer", 4000)
+
+// const company = new Company('Tech Corp', '123-456', 'Main Street')
+// company.addEmployee(emp1)
+// company.addEmployee(emp2)
+// company.addEmployee(emp3)
+
+// console.log(company.getTotalSalary()) // 12000
+// console.log(company.findEmployeeByName("Jane")) // Employee { firstName: 'Jane, ...}
+// company.removeEmployee('John')
+// console.log(company.getEmployees()) // [Employee, Employee]
+
+class Employee {
+    #salary
+    constructor(firstName, lastName, profession, salary) {
+        this.firstName = firstName; // вызывается сеттер
+        this.lastName = lastName; 
+        this.profession = profession; 
+        this.salary = salary; 
+    }
+
+    get firstName(){
+        return this._firstName
+    }
+
+    set firstName(firstName){
+        const latinLetters = 'abcdefghijklmnopqrstuvwxyz'
+        if (typeof firstName != 'string' || firstName.length < 2 || firstName.length > 50 
+                                         || !firstName.toLowerCase().split('').every(el => latinLetters.includes(el))){
+            throw new Error('Invalid firstName value')
+        }
+        this._firstName = firstName
+    }
+
+    get lastName(){
+        return this._lastName
+    }
+
+    set lastName(lastName){
+        const latinLetters = 'abcdefghijklmnopqrstuvwxyz'
+        if (typeof lastName != 'string' || lastName.length < 2 || lastName.length > 50
+                                        || !lastName.toLowerCase().split('').every(el => latinLetters.includes(el))){
+            throw new Error('Invalid lastName value')
+        }
+        this._lastName = lastName
+    }
+
+    get profession(){
+        return this._profession
+    }
+
+    set profession(profession){
+        const latinLetters = 'abcdefghijklmnopqrstuvwxyz '
+
+        if (typeof profession != 'string' || profession.length === 0
+                                          || !profession.toLowerCase().split('').every(el => latinLetters.includes(el))){
+            throw new Error('Invalid profession value')
+        }
+        this._profession = profession
+    }
+
+    get salary(){
+        return this.#salary
+    }
+
+    set salary(salary){
+        if (typeof salary != 'number' || salary <= 0 || salary > 100000){
+            throw new Error('Invalid salary value')
+        }
+        this.#salary = salary
+    }
+
+    get fullName(){
+        return `Name: ${this.firstName}, Surname: ${this.lastName}`
+    } 
+}
+
+class Company {
+    #employees = []
+    constructor(title, phone, address, employees){
+        this.title = title
+        this.phone = phone
+        this.address = address
+        this.employees = employees
+    }
+
+    get title(){
+        return this._title
+    }
+
+    set title(title){
+        if (typeof title != 'string'){
+            throw new Error('Invalid title value')
+        }
+        this._title = title
+    }
+
+    get phone(){
+        return this._phone
+    }
+
+    set phone(phone){
+        if (typeof phone != 'number'){
+            throw new Error('Invalid phone value')
+        }
+        this._phone = phone
+    }
+
+    get address(){
+        return this._address
+    }
+
+    set address(address){
+        if (typeof address != 'string'){
+            throw new Error('Invalid address value')
+        }
+        this._address = address
+    }
+
+    addEmployee(employee){
+        if (employee instanceof Employee){
+            this.#employees.push(employee)
+        }
+        else {
+            return `object ${employee} is not the instance of class Employee`
+        }
+
+    }
+
+    getEmployees(){
+        return this.#employees
+    }
+
+    getInfo(){
+        return `Компания: ${this.title} \nАдрес: ${this.address} \nКоличество сотрудников: ${this.#employees.length}`
+    }
+
+    findEmployeeByName(firstName){
+        return this.#employees.find(el => el._firstName === firstName)
+
+    }
+
+    removeEmployee(firstName){
+        const employeeIndex = this.#getEmployeeIndex(firstName)
+        if (employeeIndex === -1){
+            return `this employee ${firstName} hasn't found in our company!`
+        }
+        else {
+            delete this.#employees[employeeIndex]
+        }
+    }
+
+    #getEmployeeIndex(firstName){
+        return this.#employees.findIndex(el => el._firstName === firstName)
+    }
+
+    getTotalSalary(){        
+        return company.getEmployees().reduce((acc, el, index, arr) => {
+            acc += el.salary
+            return acc
+        }, 0)
+    }
+}
+
+const emp1 = new Employee('John', 'Doe', "Developer", 3000)
+const emp2 =  new Employee('Jane', 'Smith', "Manager", 5000)
+const emp3 =  new Employee('Mark', 'Brown', "Designer", 4000)
+
+// const company = new Company('Tech Corp', '123-456', 'Main Street') // вызывается ошибка, тк тип phone должен быть number!
+const company = new Company('Tech Corp', 123456, 'Main Street')
+
+company.addEmployee(emp1)
+company.addEmployee(emp2)
+company.addEmployee(emp3)
+
+console.log(company.getTotalSalary()) // 12000
+console.log(company.findEmployeeByName("Jane")) // Employee { firstName: 'Jane, ...}
+company.removeEmployee('John')
+console.log(company.getEmployees()) // [Employee, Employee]
