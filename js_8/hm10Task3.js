bef9e1f26065c30b250c67c605db2a557ea26779
Lina <juicelina.com@gmail.com>
2024-10-19 17:40:31 +0300

    fixed 3

--- js_8/hm10Task3.js
+++ js_8/hm10Task3.js
@@ -11,6 +11,8 @@ function getRandomArbitrary(min, max) {
     return Math.random() * (max - min) + min
 }
 
+separateLogs(3.1)
+
 function generateNumbers(number){
     const allNumbers = new Set()
     let hasSeenNumbers = 0
@@ -49,4 +51,29 @@ console.log(randomGenerator())
 console.log(randomGenerator())
 
 
+separateLogs(3.2)
 
+const createRandom = (n) => {
+    let countMin = 0;
+    let countMax = 0;
+    return () => {
+        if (countMax == n) {
+            return 'All numbers were received';
+        } else {
+            countMin++;
+            countMax++;
+            return Math.floor(getRandomArbitrary(countMin, countMax));
+        }
+  };
+}
+const  getNewRandom = createRandom(5);
+console.log(getNewRandom());
+console.log(getNewRandom());
+console.log(getNewRandom());
+console.log(getNewRandom());
+console.log(getNewRandom());
+console.log(getNewRandom());
+
+function separateLogs(num){
+    console.log(`\n----------- HomeWork ${num} ---------------\n`)
+};
