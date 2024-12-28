4a7201be9834f36e3b24a7513be4540a910dc239
Lina <juicelina.com@gmail.com>
2024-12-28 19:24:56 +0300

    fixed

--- aqa_7/src/data/credentials.ts
+++
@@ -1,9 +0,0 @@
-export const API_VALID_CREDENTIALS = {
-    USERNAME:  'aqacourse@gmail.com',
-    PASSWORD: 'password'
-}
-
-export const UI_VALID_CREDENTIALS = {
-    EMAIL:  'aqacourse@gmail.com',
-    PASSWORD: 'password'
-}
